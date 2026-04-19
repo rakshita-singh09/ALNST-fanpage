9c7b36259214c583574bdc7d005cb3471a7fcd19
rakshita-singh09 <rakshitasingh0706@gmail.com>
2026-04-19 19:21:00 -0400

    full site update

---
+++ index.js
@@ -0,0 +1,72 @@
+const r1_button = document.getElementById("r1_button");
+const r1page = document.getElementById("r1page");
+r1page.style.display = "none";
+    r1_button.addEventListener("click",event => {
+        if (r1page.style.display === "none") {
+            r1page.style.display = "block";
+        } 
+        else {
+            r1page.style.display = "none";
+        }
+    });
+
+const r2_button = document.getElementById("r2_button");
+const r2page = document.getElementById("r2page");
+r2page.style.display = "none";
+    r2_button.addEventListener("click",event => {
+        if (r2page.style.display === "none") {
+            r2page.style.display = "block";
+        } 
+        else {
+            r2page.style.display = "none";
+        }
+    });
+
+const r3_button = document.getElementById("r3_button");
+const r3page = document.getElementById("r3page");
+r3page.style.display = "none";
+    r3_button.addEventListener("click",event => {
+        if (r3page.style.display === "none") {
+            r3page.style.display = "block";
+        } 
+        else {
+            r3page.style.display = "none";
+        }
+    });
+
+const r5_button = document.getElementById("r5_button");
+const r5page = document.getElementById("r5page");
+r5page.style.display = "none";
+    r5_button.addEventListener("click",event => {
+        if (r5page.style.display === "none") {
+            r5page.style.display = "block";
+        } 
+        else {
+            r5page.style.display = "none";
+        }
+    });
+
+const r6_button = document.getElementById("r6_button");
+const r6page = document.getElementById("r6page");
+r6page.style.display = "none";
+    r6_button.addEventListener("click",event => {
+        if (r6page.style.display === "none") {
+            r6page.style.display = "block";
+        } 
+        else {
+            r6page.style.display = "none";
+        }
+    });
+
+const r7_button = document.getElementById("r7_button");
+const r7page = document.getElementById("r7page");
+r7page.style.display = "none";
+    r7_button.addEventListener("click",event => {
+        if (r7page.style.display === "none") {
+            r7page.style.display = "block";
+        } 
+        else {
+            r7page.style.display = "none";
+        }
+    });
+
